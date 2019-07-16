60139a9e9553d690c6a68bac620c3eefae3d3c57
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2019-07-16 18:35:07 -0300

    Finalização do exercício 2

--- CursoJS/Exercícios/script.js
+++ CursoJS/Exercícios/script.js
@@ -39,5 +39,65 @@ function Carregar() {
         sdc.innerText = "Boa noite!"
         img.src = "Imagens/foto_noite.png"
     }
+}
 
+function Enviar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+
+    var txtNasc = document.getElementById('txtnasc')
+    var txtSexo = document.getElementsByName('tSexo')
+	
+    var msg = document.getElementById('msg')
+
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    
+    if (Number(txtNasc.value) >= 1919 && Number(txtNasc.value) <= 1939) window.alert("Quase morrendo ein!?")
+    if (Number(txtNasc.value) < 1919) window.alert("Tá fazendo o que vivo ainda?")
+    if (Number(txtNasc.value) == ano) window.alert("Nasceu hoje?")
+
+    if (txtNasc.value.length != 4) {
+        window.alert("[ERRO] Digite um ano válido")  
+    } 
+    else if (Number(txtNasc.value) > ano) {
+        window.alert(`Nasceu em ${txtNasc.value}!? É viajante do tempo?`)
+    } else {
+        var idade = ano - Number(txtNasc.value)
+        var sexo, suf = ''
+
+        if (txtSexo[0].checked) {
+            sexo = 'Homem'
+
+            if (idade >= 0 && idade < 12) {
+                img.setAttribute('src', 'Imagens/Pessoas/menino_branco.png')
+            } else if (idade >= 12 && idade < 18) {
+                img.setAttribute('src', 'Imagens/Pessoas/adolescente_negro.png')
+            } else if (idade >= 18 && idade < 60) {
+                img.setAttribute('src', 'Imagens/Pessoas/homem_branco.png')
+            } else {
+                img.setAttribute('src', 'Imagens/Pessoas/idoso_branco.png')
+            }
+
+        } else {
+            sexo = 'Mulher'
+            suf = 'a'
+
+            if (idade > 0 && idade < 12) {
+                img.setAttribute('src', 'Imagens/Pessoas/menina_negra.png')
+            } else if (idade >= 12 && idade < 18) {
+                img.setAttribute('src', 'Imagens/Pessoas/adolescente_branca.png')
+            } else if (idade >= 18 && idade < 60) {
+                img.setAttribute('src', 'Imagens/Pessoas/mulher_branca.png')
+            } else {
+                img.setAttribute('src', 'Imagens/Pessoas/idosa_negra.png')
+            }
+        }
+
+        msg.innerHTML =  `Você é um${suf} ${sexo} e possui ${idade} anos`
+        
+        img.style.margin = "15px"
+        msg.appendChild(img)
+    }
 }

49d3b4a089b7a8c7fe7a98dc5137a1ca6d003c75
JanderSilv <jander.500@hotmail.com>
2019-08-25 21:29:10 -0300

    Introdução ao App e exercício 7

--- CursoJS/Exercícios/script.js
+++ CursoJS/Exercícios/script.js
@@ -4,41 +4,44 @@ function Atualizar() {
 
 function Carregar() {
 
-    var msg = document.getElementById('msg')
-    var sdc = document.getElementById('sdc')
-    var img = document.getElementById('imagem')
+    var msg = document.getElementById('msg');
+    var sdc = document.getElementById('sdc');
+    var img = document.getElementById('imagem');
 
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
 
     msg.innerHTML = `* Agora são ${hora} horas e ${minuto} minutos *`    
 
     if ((hora >= 0 && minuto >= 0) && (hora < 5 && minuto < 60)) {
         document.body.style.background = '#434340'
         sdc.innerHTML = "Boa Madrugada!"
-        img.src = 'Imagens/foto_madrugada.png'
+        img.src = 'Imagens/Horários/foto_madrugada.png'
     }
     if ((hora >= 5 && minuto >= 0) && ((hora < 11 && minuto < 60) || (hora < 12 && minuto < 30))) {
         document.body.style.background = '#F3EF68'
         sdc.innerText = "Bom dia!"
-        img.src = "Imagens/foto_manha.png"
+        img.src = "Imagens/Horários/foto_manha.png"
     }
     if (((hora >= 11 && minuto >= 30) && (hora < 12 && minuto < 60)) || (hora >= 12 && minuto >= 0) && (hora >= 12 && hora < 14)) {
         document.body.style.background = '#EF5121'
         sdc.innerText = "Bom almoço!"
-        img.src = "Imagens/foto_almoco.png"
+        img.src = "Imagens/Horários/foto_almoco.png"
     }
     if ((hora >= 13 && hora < 18) && (hora < 18 && minuto < 60)) {
         document.body.style.background = '#F79238'
         sdc.innerText = "Boa tarde!"
-        img.src = "Imagens/foto_tarde.png"
+        img.src = "Imagens/Horários/foto_tarde.png"
     }
     if ((hora >= 18 && (hora < 24 && minuto < 60))) {
-        document.body.style.background = '#2C2C2B'
-        sdc.innerText = "Boa noite!"
-        img.src = "Imagens/foto_noite.png"
+        document.body.style.background = '#2C2C2B';
+        sdc.innerText = "Boa noite!";
+        img.src = "Imagens/Horários/foto_noite.png";
     }
+
+    // setInterval(Carregar,10000);
+    // setTimeOut(Carregar, 10000); Roda uma única vez em um delay
 }
 
 function Enviar() {
---
+++ CursoJS/Exercícios/script2.js
@@ -0,0 +1,34 @@
+// Verificar se vetor possui item
+
+// function temHabilidade(skills) {  // código aqui
+//     if (skills.indexOf('Javascript') >= 0) return true;
+//     else false;
+// }
+
+// var skills = ["Javascript", "ReactJS", "React Native"];
+
+// console.log(temHabilidade(skills));
+
+// 
+var usuarios = [  
+    {
+        nome: "Diego",    
+        habilidades: ["Javascript", "ReactJS", "Redux"]  
+    },  
+    
+    {    
+        nome: "Gabriel",    
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]  
+    }];
+
+    for (const nome of usuarios) {
+        console.log(nome);
+    }
+
+    // array.forEach(element => {
+        
+    // });
+
+    // for (const pos of usuarios) {
+        
+    // }
